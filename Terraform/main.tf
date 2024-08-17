f60a07c12c0d65a06b470ad33909e4bb4a3eb7e0
RishwanaBegam <149790091+RishwanaBegam@users.noreply.github.com>
2024-08-17 06:43:10 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-04a81a99f5ec58529"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+    #subnet_id = "subnet-0600ab3ed0c5fd24c"   # can select default subnet or specify desired subnet id
+    key_name = "aws-login"
+}
