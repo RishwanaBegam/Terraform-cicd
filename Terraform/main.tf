0b9ba631a043594134f963910b8afea5cad877d6
RishwanaBegam <149790091+RishwanaBegam@users.noreply.github.com>
2024-08-18 18:43:56 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
+resource "aws_s3_bucket" "rishwana-remote-backend-test" {
+  bucket = "rishwana-backend-test"        # specify bucket name
+}
+
 resource "aws_instance" "example" {
     ami           = "ami-04a81a99f5ec58529"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
