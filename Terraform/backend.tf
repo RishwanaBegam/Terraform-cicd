72e5e27c67cb6675ae47dd482a02362adff8e97b
RishwanaBegam <149790091+RishwanaBegam@users.noreply.github.com>
2024-08-18 18:44:27 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,4 +1,4 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "rishwana-backend-test"
     region = "us-east-1"
@@ -6,4 +6,4 @@ terraform {
     encrypt        = true
     dynamodb_table = "terraform-lock"    #dynamoDB table name
   }
-}
+}*/
